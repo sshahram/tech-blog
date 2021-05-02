5d4a4dc9cbb6c28531f797c3f2721e0680e01417
sshahram <shirin.shahram23@gmail.com>
2021-05-02 12:07:29 -0700

    extend maxAge for cookies

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
     secret: 'secret',
-    cookie: {maxAge: 60000},
+    cookie: {maxAge: 600000},
     rolling: true,
     resave: false,
     saveUninitialized: true,
